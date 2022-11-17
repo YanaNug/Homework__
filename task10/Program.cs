2b55360ea5ed3cc0d2bb380e4ccf52a657fa4f24
Yana <yana229910nug@yandex.ru>
2022-11-17 23:04:45 +0300

    дополнила другим решением

--- task10/Program.cs
+++ task10/Program.cs
@@ -4,8 +4,14 @@
 // 782 -> 8
 // 918 -> 1
 
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Наше случайное трехзначное число = {number}");
+// int numberA = number/10;
+// int numberC = numberA%10;
+// Console.WriteLine($"Вторая цифра данного числа - {numberC}");
+
+
+
 int number = new Random().Next(100, 1000);
-Console.WriteLine($"Наше случайное трехзначное число = {number}");
-int numberA = number/10;
-int numberC = numberA%10;
-Console.WriteLine($"Вторая цифра данного числа - {numberC}");
+string num = number.ToString();
+Console.WriteLine($"Вторая цифра числа {number} - {num[1]}");
