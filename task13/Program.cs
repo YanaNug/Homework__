ea1820e5788d5be8d4550f62c03c192af308062d
Yana <yana229910nug@yandex.ru>
2022-11-17 23:01:15 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string num = number.ToString();
+int n = num.Length;
+if(n<3) {
+    Console.WriteLine("Третьей цифры нет");
+}
+else {
+    Console.WriteLine($"Третья цифра числа {number} - {num[2]}");
+}
