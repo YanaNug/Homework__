e596f3775f2b44d9f826835c9f8f7eb8c2329018
Yana <yana229910nug@yandex.ru>
2022-11-26 15:36:39 +0300

    task29 - заполнение массива

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array = new int[8];
+for(int i=0;i<array.Length; i++){
+  array[i] = new Random().Next(1, 1000);
+   }
+Console.WriteLine($"[" + string.Join(",", array) +"]");
+
+// Console.WriteLine("Введите числа:");
+// int[] array = new int[8];
+// for(int i=0;i<array.Length; i++){
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine($"[" + string.Join(",", array) +"]");
+
