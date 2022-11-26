39ba62959542fbf5d297e3849af6dfc7483c1304
Yana <yana229910nug@yandex.ru>
2022-11-26 19:05:55 +0300

    task36 - сумма элементов массива на нечетных индексах

---
+++ task36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях(индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] FillArray(int size)
+{
+int[] array = new int[15];
+for(int i=0;i<array.Length; i++){
+  array[i] = new Random().Next(1,100); //  array[i] = new Random().Next(); 
+   }
+return array;
+}
+int[] array = FillArray(15);
+Console.WriteLine($"[" + string.Join(",", array) +"]");
+
+       int num = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(i% 2 == 1)
+    {
+        num = num + array[i];
+    }
+}
+  Console.WriteLine( num);
