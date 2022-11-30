f98badc483dd3b77f62ed0b8d7743e6e99670198
Yana <yana229910nug@yandex.ru>
2022-11-30 22:11:43 +0300

    task41 - из введенных пользователем чисел найти все что больше 0

---
+++ task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите число элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите элементы массива:");
+int[] array = new int[n];
+
+for(int i=0;i<array.Length; i++)
+{
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"[" + string.Join(", ", array) +"]"); 
+
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    int num = array[i];
+    if(num > 0){
+        count++;
+    }
+}
+Console.WriteLine($"Kоличество чисел больше 0: " + count);
