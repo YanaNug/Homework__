75e1f2bc81d68ea6d6a046f539e645d0aafb9bba
Yana <yana229910nug@yandex.ru>
2022-12-04 23:43:59 +0300

    task52 - среднее арифм. каждого столбца двумерного массива

---
+++ task52/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите  m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int [,] arr = new int [m,n];
+for(int i = 0; i < m; i++)
+{
+for(int j = 0; j < n; j++)
+{
+arr[i,j] = new Random().Next(0,36);
+Console.Write(arr[i,j] + " ");
+}
+Console.WriteLine();
+}
+
+Console.WriteLine(" ");
+
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
